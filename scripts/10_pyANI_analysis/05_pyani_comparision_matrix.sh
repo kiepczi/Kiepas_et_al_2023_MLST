eaa93940960a0106e7271460cede2d9e1d56d8f6
Angelika Kiepas <angelikakiepas@Angelikas-MBP.lan>
2023-08-24 11:50:36 +0100

    remove files

--- scripts/10_pyANI_analysis/05_pyani_comparision_matrix.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#Get pyANI matrices comparisions
-
-
-for i in {1..117}
-do
-   pyani report -o ~/Desktop/Kiepas_et_al_2023_MLST/data/pyani_analysis/connected_components_matrices --formats=stdout --run_matrices $i
-done
